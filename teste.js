36559610070e2f99532984885567cf56e65b77d0
geovanitchitengula51@gmail.com <geovanitchitengula51@gmail.com>
2022-10-14 16:52:37 +0100

    Sunda Alteração

---
+++ teste.js
@@ -0,0 +1,19 @@
+alert(
+    'Bem - Vindo'
+);
+var idade = 17;
+var nome = "Geovany Tchitengula"
+var n1= 3;
+var n2=3;
+var n3 =3
+var mul = n1 * n2 * n3;
+document.write(" O Resultado Da Multiplicação é " , mul ,"<br>");
+document.write("<br>")
+
+
+if (mul % 2==0) {
+    document.write("\n O valor é Par" , mul);
+}
+else {
+    document.log("\n O valor é Impar", mul);
+}
